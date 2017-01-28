9069dab0dc6aabb3b2a0d45a37915b2fe983b52d
bayangarkov <baiangarkov@gmail.com>
2017-01-28 12:08:58 +0200

    Testing

---
+++ 03-ExactProductOfRealNumebrs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ExactProductOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+
+            decimal finalSUm = 1m;
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                decimal inputFromLoop = decimal.Parse(Console.ReadLine());
+
+                finalSUm = finalSUm * inputFromLoop;
+            }
+
+            Console.WriteLine(finalSUm);
+        }
+    }
+}
---
+++ 04-Transport/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputPeople = int.Parse(Console.ReadLine());
+
+            float capacity = 4 + 8 + 12;
+
+            float sum = inputPeople / capacity;
+
+            Console.WriteLine(Math.Ceiling(sum));
+        }
+    }
+}
