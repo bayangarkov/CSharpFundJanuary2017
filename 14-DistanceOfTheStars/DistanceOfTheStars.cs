0b986526fb87c610568ed45e10ac2888cf2f072e
bayangarkov <baiangarkov@gmail.com>
2017-01-29 15:31:44 +0200

    new project

---
+++ 14-DistanceOfTheStars/DistanceOfTheStars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _14_DistanceOfTheStars
+{
+    class DistanceOfTheStars
+    {
+        static void Main()
+        {
+            decimal oneLightYear = 9450000000000m;
+
+            decimal toStar = (decimal)4.22 * oneLightYear;
+            decimal toCenterOfGalaxy = (decimal)26000 * oneLightYear;
+            decimal diameterOfMilkyWay = (decimal)100000 * oneLightYear;
+            decimal toTheVeryEdge = (decimal)46500000000 * oneLightYear;
+
+            Console.WriteLine("{0:e2}",toStar);
+            Console.WriteLine("{0:e2}", toCenterOfGalaxy);
+            Console.WriteLine("{0:e2}", diameterOfMilkyWay);
+            Console.WriteLine("{0:e2}", toTheVeryEdge);
+        }
+    }
+}
