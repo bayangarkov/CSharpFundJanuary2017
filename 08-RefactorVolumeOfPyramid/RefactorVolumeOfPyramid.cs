53e569ef58aaa8c05cf77efcf318afb33557b8c0
bayangarkov <baiangarkov@gmail.com>
2017-01-29 11:56:16 +0200

    apply before and after refactoring code

--- 08-RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
+++ 08-RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -10,6 +10,20 @@ namespace _08_RefactorVolumeOfPyramid
     {
         static void Main()
         {
+            //Before refactoring
+
+           // double dul, sh, V = 0;
+           // Console.Write("Length: ");
+           // dul = double.Parse(Console.ReadLine());
+           // Console.Write("Width: ");
+           // sh = double.Parse(Console.ReadLine());
+           // Console.Write("Heigth: ");
+           // V = double.Parse(Console.ReadLine());
+           // V = (dul + sh + V) / 3;
+           // Console.WriteLine("Pyramid Volume: {0:F2}", V);
+
+            //After refactoring
+
             double length, width, volume = 0.0d;
 
             Console.Write("Length: ");
