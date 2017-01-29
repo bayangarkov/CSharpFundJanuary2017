99cd808cc5d0bf1cbbb6acb5521527204bfd4fae
bayangarkov <baiangarkov@gmail.com>
2017-01-29 13:26:13 +0200

    new project

---
+++ 12-ExchangeVariableValues/ExchangeVariableValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_ExchangeVariableValues
+{
+    class ExchangeVariableValues
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = a;
+
+            a = b;
+            b = c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
