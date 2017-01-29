a462f03fb6468c2f99288ece0644be263d5027e7
bayangarkov <baiangarkov@gmail.com>
2017-01-29 16:31:27 +0200

    From Terabyte to bit convertor

---
+++ 16-FromTerabytesToBits/FromTerabytesToBitsConvertion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_FromTerabytesToBits
+{
+    class FromTerabytesToBitsConvertion
+    {
+        static void Main()
+        {
+            var inputTerabyte = double.Parse(Console.ReadLine());
+
+            double convertToByte = (double)inputTerabyte * 1024 * 1024 * 1024 * 1024 * 8;
+
+            Console.WriteLine(convertToByte);
+        }
+    }
+}
