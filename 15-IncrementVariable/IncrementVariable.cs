145cb579af86fa725ed06eec78119fcc8e1c959b
bayangarkov <baiangarkov@gmail.com>
2017-01-29 16:04:51 +0200

    Project for counting overflow of an given type

---
+++ 15-IncrementVariable/IncrementVariable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_IncrementVariable
+{
+    class IncrementVariable
+    {
+        static void Main()
+        {
+            var inputNum = double.Parse(Console.ReadLine());
+
+            double overflowCounter = Convert.ToDouble(Math.Floor(inputNum / 256));
+
+            double formulaForDifference = (inputNum - (overflowCounter * 256));
+
+            if ((inputNum > 0) && (inputNum < 256))
+            {
+                Console.WriteLine(inputNum);
+            }
+            else
+            {
+                Console.WriteLine($"{formulaForDifference}\nOverflowed {overflowCounter} times");
+            }
+
+            // The logic is : the given num from the console we devide by 256 to see how many "overflows" we have.
+            // Next to check how many leftover digits we have , i multiply the counter * 256. 
+            // Then that result is been drawn from inputNumber and the result is how many leftover we have.
+        }
+    }
+}
