9dc95e832f9b2f20518d317e6f9c73178e8728c2
bayangarkov <baiangarkov@gmail.com>
2017-01-29 10:07:38 +0200

    Uploading new project

---
+++ 05-SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_SpecialNumbers
+{
+    class SpecialNumbers
+    {
+        static void Main()
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= inputNum; num++)
+            {
+                var currentNumber = num;
+                var sum = 0;
+
+                foreach (var symbol in currentNumber.ToString())
+                {
+                    var digit = symbol - '0';
+                    sum += digit;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine("{0} -> True",currentNumber);
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> False",currentNumber);
+                }
+
+            }
+        }
+    }
+}
