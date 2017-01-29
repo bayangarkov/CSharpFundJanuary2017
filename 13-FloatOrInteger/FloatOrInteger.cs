efab53fcd57b661c91a9f81d29fc9620d745b6b3
bayangarkov <baiangarkov@gmail.com>
2017-01-29 13:46:36 +0200

    new upload

---
+++ 13-FloatOrInteger/FloatOrInteger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_FloatOrInteger
+{
+    class FloatOrInteger
+    {
+        static void Main()
+        {
+            var inputNum = double.Parse(Console.ReadLine());
+
+            var toInt = Convert.ToInt32(Math.Round(inputNum));
+
+            Console.WriteLine(toInt);
+        }
+    }
+}
